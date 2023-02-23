ab702f6a31d8143fda682c520a27040998c41861
yeswanth enjamuri <118601644+MRYESHU8121@users.noreply.github.com>
2023-02-23 15:33:22 +0530

    Create main.py
    yeswanth mr
    Real-Time Facal emotions ,gender using open cv and DeepFace in Python

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 11:50:25 2023
+
+@author: YESHU
+"""
+
+import cv2
+import numpy as np
+from deepface import DeepFace
+
+face_cascade = cv2.CascadeClassifier('E:/arificial classs/Day 5/haarcascade_frontalface_alt.xml')
+cap = cv2.VideoCapture(0)
+scaling_factor = 1
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=3)
+    for (x, y, w, h) in faces:
+        face = frame[y:y+h, x:x+w]
+        emotions = DeepFace.analyze(face, actions=['emotion'],enforce_detection=False)
+        gender = DeepFace.analyze(face, actions=['gender'],enforce_detection=False)
+
+        emotion_text = "Emotion: " + emotions[0]['dominant_emotion']
+        gender_text = "Gender: " + gender['gender']
+        print(emotion_text)
+        print(gender_text)
+        
+        cv2.putText(frame, emotion_text, (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+      
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
